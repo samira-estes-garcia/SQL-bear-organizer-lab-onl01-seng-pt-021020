be68cb3463fe8c59acc8842b252bf9c2e9a2e746
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-04-08 23:32:52 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,8 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 5, "male", "brown", "nice", 1)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Rowdy", 6, "male", "brown", "nice", 1)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Tabitha", 7, "male", "brown", "nice", 1)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Sergeant Brown", 5, "male", "brown", "nice", 1)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Melissa", 5, "male", "brown", "nice", 1)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Grinch", 5, "male", "brown", "nice", 1)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Wendy", 5, "male", "brown", "nice", 1)
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, NULL, 5, "male", "brown", "nice", 1)

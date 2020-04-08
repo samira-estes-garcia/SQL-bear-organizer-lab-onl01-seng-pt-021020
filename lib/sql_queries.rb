41d78f14932cfb8ec14c6012638758b7302573a4
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-04-08 23:44:42 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  'SELECT bears.name, bears.age FROM bears GROUP BY gender;'
+  'SELECT bears.name, bears.age FROM bears;'
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order

283ae341344a1c3e6a6951b1dd09362916316a1e
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-04-08 23:59:01 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT bears.name, bears.age FROM bears WHERE age <  LIMIT 1;"
+  "SELECT bears.name, bears.age FROM bears WHERE age < 5 LIMIT 1;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
